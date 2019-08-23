c915b3baf9672de5eed226f53a5f8d9c7919b826
Szymon Czapkowski <szymon.czapkowski@gmail.com>
2019-08-23 11:30:52 +0200

    done

---
+++ src/GlobalStyles.js
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    background-color: #E0E6EE;
+    font-family: 'Ubuntu', sans-serif;
+  }
+`
