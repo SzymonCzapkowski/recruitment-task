e1801bb66194c30271b8e2b05b03c3109573b164
Szymon Czapkowski <szymon.czapkowski@gmail.com>
2019-08-26 01:12:45 +0200

    some

--- src/components/Add/Add.jsx
+++ src/components/Add/Add.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { AddContainer, AddStrap, AddOpen, AddButton } from './AddStyles';
+import { AddContainer, AddStrap, AddOpen, AddButton, } from './AddStyles';
 import ARROWOPEN from '../../Assets/ARROW OPEN.jsx'
 import ARROWCLOSE from '../../Assets/ARROW CLOSE.jsx'
 import SEARCH from '../../Assets/SEARCH.jsx'
+import Episode from '../MainPage/Episode/Episode.jsx'
 
 
 class Add extends React.Component {
@@ -11,8 +12,8 @@ class Add extends React.Component {
         super(props);
         this.state = { 
             isOpened : false,
-            title:'',
-            planet:'',
+            newItem:'',
+            list:[{id:1}, {id:2}]
         };
         this.open = this.open.bind(this);
         this.close = this.close.bind(this);
@@ -30,10 +31,40 @@ class Add extends React.Component {
         this.setState({ isOpened: false })
         
     };
+
+    updateInput(key, value){
+        this.setState({
+            [key]: value
+        });
+    };
+
+    addItem(){
+        const newItem={
+            id: 1 + Math.random(),
+            value: this.state.newItem.slice()
+        };
+        const list = {...this.state.list};
+
+        list.push(newItem);
+
+        this.setState({
+            list,
+            newItem:''
+        });
+    };
     
     render() {
     return (
-        
+    <>
+    <ul>
+    {this.state.list.map( item => {
+        return(
+        <li style={{ listStyleType: "none" }} key={item.id}><Episode key={item.id}>
+        {item.value}
+        </Episode></li>
+        )
+    })}
+    </ul>   
     <AddContainer>        
         <AddStrap> <p>Add Movie</p>            
             <div className="arrows">
@@ -44,25 +75,27 @@ class Add extends React.Component {
         { this.state.isOpened ?
         <AddOpen>
             <div className="addDivContainer" onSubmit={this.onSubmit}>
-                <form className="addDiv" >
+                <form className="addDiv" type="text" value={this.state.newItem} 
+                onChange={e => this.updateForm('newItem', e.target.value)} >
                     <p>Movie Title</p>
                     <input className="addInput" placeholder="Please enter the tittle of the movie" />           
                 </form>
                 <form className="addDiv">
                     <p>Add Planet</p>
-                    <input className="addInput"  placeholder="Search for the the planet in database" />
+                    <input className="addInput" placeholder="Search for the the planet in database" />
                     <button>
                         <SEARCH/>
                     </button>
                 </form>
             </div>
-            <AddButton>
+            <AddButton onClick={() => this.addItem()}>
                 ADD MOVIE
             </AddButton>            
         </AddOpen> 
         :null }             
         
     </AddContainer>
+    </>
     );
 }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { GlobalStyles } from './GlobalStyles';
 import Header from './Header/Header.jsx';
 import Footer from './Footer/Footer.jsx';
-import MainPage from './MainPage/MainPage.jsx';
 import Add from './Add/Add.jsx';
-import Episode from './MainPage/Episode/Episode.jsx';
+
 
 function App() {
   return (
@@ -13,10 +12,6 @@ function App() {
     <GlobalStyles />
     <Router>
     <Header />
-      <Switch>
-        <Route path="/mainpage" component={MainPage} />      
-        <Route path="/" component={Episode}/>
-      </Switch>
     <Add />
     <Footer />
     </Router>  
